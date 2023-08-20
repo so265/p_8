601edd09e081deee1516541673f671d403f20f60
so265 <undia42@gmail.com>
2023-08-20 23:20:23 +0200

    creation collapse file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../components/Banner';
+import Collapse from '../components/Collapse'
 
 function About() {
   return (
     <div>
       <Banner imageSrc="montagne.png" />
+      <Collapse />
       
     </div>
   );
