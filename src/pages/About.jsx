2779f9024ffb7f148fd95009be50c7bb15943453
so265 <undia42@gmail.com>
2023-08-23 15:48:47 +0200

    collapse

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,3 @@
-/*import React from 'react';
-import Banner from '../components/Banner';
-import Collapse from '../components/Collapse'
-
-function About() {
-  return (
-    <div>
-      <Banner imageSrc="montagne.png" />
-      <Collapse />
-      
-    </div>
-  );
-}
-
-export default About;*/
-
 import React from 'react';
 import Banner from '../components/Banner';
 import Collapse from '../components/Collapse.jsx'
