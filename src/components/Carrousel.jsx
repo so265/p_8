2ea4cd689a8e43fa789881f0fa25980b2c6eed99
so265 <undia42@gmail.com>
2023-08-28 00:56:03 +0200

    carousel creation

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
-import '../styleComponents/Carrousel.scss'
+import React, { useState } from 'react';
+import '../styleComponents/Carrousel.scss';
 import ArrowLeft from '../assets/chevron-carrousel-left.png'
 import ArrowRight from'../assets/chevron-carrousel-right.png'
 
-function Carrousel() {
+
+function Carrousel({ Housing }) {
+  const [currentIndex, setCurrentIndex] = useState(0); /*cette variable permet de savoir quelle image est actuellement affichée dans le carrousel, et c'est ce qui permet de faire défiler les images en avant ou en arrière.*/ 
+
+  const goToPrevious = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? Housing.length - 1 : prevIndex - 1)); //Décrémente l'index pour afficher l'image précédente
+  };
+
+  const goToNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === Housing.length - 1 ? 0 : prevIndex + 1)); //Incrémente l'index pour afficher l'image suivante
+  };
+
+  const showArrows = Housing.length > 1; // Vérifie si j'ai plus d'une image pour afficher les flèches
+  const showCounter = Housing.length > 1; // Vérifie si vous avez plus d'une image pour afficher le compteur
+
   return (
-    <div>
-    <div><img className='chevron-left' src={ArrowLeft} alt="Fléche gauche" /></div>
-    <div><img className='chevron-left' src={ArrowRight} alt="Fléche droite" /></div>
+    <div className="slider">
+      <img className="slider-image" src={Housing[currentIndex]} alt={`Vue de la chambre ${currentIndex + 1}`} /> {/* Affiche l'image actuelle */}
+{/* Si showArrows est vrai, les flèches sont affichées */}
+      {showArrows && ( 
+        <>
+          <img
+            className='slider-arrow chevron-left'
+            src={ArrowLeft}
+            alt="show previous slider"
+            onClick={goToPrevious} // Appelle la fonction goToPrevious lors du clic sur la flèche gauche
+          />
+
+          <img
+            className='slider-arrow chevron-right'
+            src={ArrowRight}
+            alt="show next slider"
+            onClick={goToNext} // Appelle la fonction goToPrevious lors du clic sur la flèche droite
+          />
+        </>
+      )}
+
+      {showCounter && <p className='slideCount'>{currentIndex + 1} / {Housing.length}</p>} {/* Affiche le compteur si showCounter est vrai */}
     </div>
-  )
+  );
 }
 
-export default Carrousel
+export default Carrousel;
--- src/pages/LocationPage.jsx
+++ src/pages/LocationPage.jsx
@@ -3,8 +3,7 @@ import { Navigate, useParams } from 'react-router-dom';
 import Housing from './Housing.json';
 import Carrousel from '../components/Carrousel.jsx';
 
-import '../stylePages/Location.scss'
-
+import '../stylePages/Location.scss';
 
 function LocationPage() {
   const { id } = useParams();
@@ -12,16 +11,12 @@ function LocationPage() {
   const location = Housing.find(item => item.id === id);
 
   if (!location) {
-    return <Navigate to="somewhere/else" />
+    return <Navigate to="somewhere/else" />;
   }
 
   return (
-    
     <div>
-      <Carrousel />
-      {location.pictures.map((picture, index) => (
-        <img key={index} src={picture} alt={`logements ${index}`} className='location-images' />
-      ))}
+      <Carrousel Housing={location.pictures} />
       <h1 className='title-images'>{location.title}</h1>
       <p className='city-location'>{location.location}</p>
     </div>
