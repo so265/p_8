460a5856a098dcc0c9d84850f75972f38baaf55c
so265 <undia42@gmail.com>
2023-08-31 01:26:44 +0200

    margin collapse

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -26,17 +26,18 @@ function About() {
 			"content": "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
 		}
 	]
-  return (
-    <div>
-        <Banner imageSrc="montagne.png" /> 
-        <div className="accordion" >
-    {accordionData.map(({ id, title, content }) => (
-      <Collapse key={id} title={title} content={content} />
-    ))}
-  </div>
-    </div>
-);
-}
+	return (
+		<div className="about-container">
+		  <Banner imageSrc="montagne.png" /> 
+		  <div className="accordion">
+			{accordionData.map(({ id, title, content }) => (
+			  <Collapse key={id} title={title} content={content} />
+			))}
+		  </div>
+		</div>
+	  );
+	}
+	
+	export default About;
 
 
-export default About;
